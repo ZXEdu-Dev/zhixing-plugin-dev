11fbc43b4aefe5849ebcdf9ff883fbf180aceb1e
ZX <86369971+ZXEducation@users.noreply.github.com>
2021-07-09 13:44:31 +0800

    Add file dist/button.es.js in plugin 1413373443352637442

---
+++ 1413373443352637442/dist/button.es.js
@@ -0,0 +1,28 @@
+const Button = "_Button_en3w4_1";
+var styles = {
+  Button
+};
+const Widget = ({ input, onExecuted }) => {
+  const [text, setText] = React.useState();
+  const [output, setOutput] = React.useState();
+  React.useEffect(() => {
+    try {
+      const inputData = JSON.parse(input);
+      if (!!inputData.text) {
+        setText(inputData.text);
+      }
+      if (!!inputData.output) {
+        setOutput(inputData.output);
+      }
+    } catch (e) {
+    }
+  }, [input]);
+  return /* @__PURE__ */ React.createElement("button", {
+    type: "button",
+    onClick: () => {
+      onExecuted(output);
+    },
+    className: styles.Button
+  }, text);
+};
+export default Widget;
