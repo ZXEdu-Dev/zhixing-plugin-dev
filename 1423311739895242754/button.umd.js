185b61fa44967c8dea6dd29f325e6f2a359b8397
ZX <86369971+ZXEducation@users.noreply.github.com>
2021-08-05 23:55:40 +0800

    Add file button.umd.js in plugin 1423311739895242754

---
+++ 1423311739895242754/button.umd.js
@@ -0,0 +1,34 @@
+(function(global, factory) {
+  typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory() : typeof define === "function" && define.amd ? define(factory) : (global = typeof globalThis !== "undefined" ? globalThis : global || self, global.Button = factory());
+})(this, function() {
+  "use strict";
+  const Button = "_Button_en3w4_1";
+  var styles = {
+    Button
+  };
+  const Widget = ({ options }) => {
+    const { input, onExecuted } = options;
+    const [text, setText] = React.useState();
+    const [output, setOutput] = React.useState();
+    React.useEffect(() => {
+      try {
+        const inputData = JSON.parse(input);
+        if (!!inputData.text) {
+          setText(inputData.text);
+        }
+        if (!!inputData.output) {
+          setOutput(inputData.output);
+        }
+      } catch (e) {
+      }
+    }, [input]);
+    return /* @__PURE__ */ React.createElement("button", {
+      type: "button",
+      onClick: () => {
+        onExecuted(output);
+      },
+      className: styles.Button
+    }, text);
+  };
+  return Widget;
+});
